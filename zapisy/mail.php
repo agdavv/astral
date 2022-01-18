ea0ec6855598b1acd49d0a8875ae47999aa5ca31
Voxuuu <hajdecki.dawid@gmail.com>
2022-01-18 00:54:58 +0000

    formularz 1.3

--- zapisy/mail.php
+++ zapisy/mail.php
@@ -1,11 +1,29 @@
 <?php
+    $eol = "\r\n";
+    $separator = md5(time());
     $to = "astralwars@hajdecki.com"; // replace this mail with yours
-    $from =   "aa";  //$_SERVER['PHP_SELF']." ".$_POST["emailceo"];
-    $fname =  "aa";  //$_POST["name"];
-    $email =  "aa";  //$_POST["email"];
-    $lname =  "aa";  //$_POST["subject"];
-    $headers ="aa";  //"From: $from";
-    $message ="aa";  //$_POST["message"];
+    $filenameee =  $_FILES['file']['name'];
+    $fileName = $_FILES['file']['tmp_name']; 
+    
+    $content = file_get_contents($fileName);
+    $content = chunk_split(base64_encode($content));
+
+    $body = "--" . $separator . $eol;
+    $body .= "Content-Type: text/plain; charset=\"iso-8859-1\"" . $eol;
+    $body .= "Content-Transfer-Encoding: 8bit" . $eol;
+
+    $body .= "--" . $separator . $eol;
+    $body .= "Content-Type: application/octet-stream; name=\"" . $filenameee . "\"" . $eol;
+    $body .= "Content-Transfer-Encoding: base64" . $eol;
+    $body .= "Content-Disposition: attachment" . $eol;
+    $body .= $content . $eol;
+    $body .= "--" . $separator . "--";
+    //$from =   "aa";  //$_SERVER['PHP_SELF']." ".$_POST["emailceo"];
+    //$fname =  "aa";  //$_POST["name"];
+    //$email =  "aa";  //$_POST["email"];
+    //$lname =  "aa";  //$_POST["subject"];
+    //$headers ="aa";  //"From: $from";
+    //$message ="aa";  //$_POST["message"];
 
     $body = "User Message \n";
     $body .= " \n\n\t Name: ".$name;
