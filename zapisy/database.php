a61a185d5eada572d2b33f72d168770d9df9442b
Voxuuu <hajdecki.dawid@gmail.com>
2022-01-18 22:54:59 +0000

    a

--- zapisy/database.php
+++ zapisy/database.php
@@ -8,6 +8,5 @@ $dbName = "u655902916_Astral";
 $conn = mysqli_connect($servername, $dbUsername, $dbPassword, $dbName);
 
 if(!$conn) {
-    var_dump("hey");
     die("Connection Failed: ".mysqli_connect_error());
 }
