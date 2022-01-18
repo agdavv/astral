db576b05ca0323376f076ba0908fbcad340a61c1
Voxuuu <hajdecki.dawid@gmail.com>
2022-01-18 23:35:04 +0000

    Update zapisy.php

--- zapisy/zapisy.php
+++ zapisy/zapisy.php
@@ -1,2 +1,17 @@
 <?php
 var_dump("hey");
+
+    $druzynaNazwa       = $_POST['druzynaNazwa'];
+    $druzynaFanpage     = $_POST['druzynaFanpage'];
+    $druzynaLogo        = $_POST['druzynaLogo'];
+    $ceoEmail           = $_POST['ceoEmail'];
+    $ceoDiscord         = $_POST['ceoDiscord'];
+    $steamKapitan       = $_POST['steamKapitan'];
+    $steamGracz2        = $_POST['steamGracz2'];
+    $steamGracz3        = $_POST['steamGracz3'];
+    $steamGracz4        = $_POST['steamGracz4'];
+    $steamGracz5        = $_POST['steamGracz5'];
+    $steamStandIn1      = $_POST['steamStandIn1'];
+    $steamStandIn2      = $_POST['steamStandIn2'];
+    $steamTrener        = $_POST['steamTrener'];
+    $steamCeo           = $_POST['steamCeo'];
