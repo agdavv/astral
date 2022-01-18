e8e7b4df18418b8c915da8a639f338052d10ec66
Voxuuu <hajdecki.dawid@gmail.com>
2022-01-18 23:40:16 +0000

    Update zapisy.php

--- zapisy/zapisy.php
+++ zapisy/zapisy.php
@@ -17,14 +17,27 @@
     $steamTrener        = $_POST['steamTrener'];
     $steamCeo           = $_POST['steamCeo'];
 
+    if(empty($druzynaNazwa) || empty($druzynaFanpage) || empty($druzynaLogo) || empty($ceoEmail) || empty($ceoDiscord) || empty($ceoEmail) || empty($steamKapitan) || empty($steamGracz2) || empty($steamGracz2) || empty($steamGracz3) || empty($steamGracz4) || empty($steamGracz5)){
+        header("location: ../index.html?error=emptyfields");
+        exit();
+    }else{
+        $sql = "
+            INSERT INTO users (druzynaNazwa,druzynaFanpage,druzynaLogo,ceoEmail,ceoDiscord,steamKapitan,steamGracz2,steamGracz3,steamGracz4,steamGracz5,steamStandIn1,steamStandIn2,steamTrener,steamCeo) 
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        $statement = mysqli_stmt_init($conn);
+        if(!mysqli_stmt_prepare($statement, $sql)){
+            header("location: ../index.php?error=sqlerror");
+            exit();
+        }else{
 
-    $sql = "
-        INSERT INTO users (druzynaNazwa,druzynaFanpage,druzynaLogo,ceoEmail,ceoDiscord,steamKapitan,steamGracz2,steamGracz3,steamGracz4,steamGracz5,steamStandIn1,steamStandIn2,steamTrener,steamCeo) 
-        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-    $statement = mysqli_stmt_init($conn);
-
-    var_dump("hey");
-
+            mysqli_stmt_bind_param($statement, "ssssssssssssss", $druzynaNazw ,$druzynaFanp ,$druzynaLogo ,$ceoEmail ,$ceoDiscord ,$steamKapita ,$steamGracz2 ,$steamGracz3 ,$steamGracz4 ,$steamGracz5 ,$steamStandI ,$steamStandI ,$steamTrener ,$steamCeo);
+            mysqli_stmt_execute($statement);
+            header("location: ../index.php?signup=success");
+            exit();
+        }
+    }
+    mysqli_stmt_close($statement);
+    mysqli_close($conn);
 
 /*
 ,druzynaNazwa,druzynaFanpage,druzynaLogo,ceoEmail,ceoDiscord,steamKapitan,steamGracz2,steamGracz3,steamGracz4,steamGracz5,steamStandIn1,steamStandIn2,steamTrener,steamCeo
